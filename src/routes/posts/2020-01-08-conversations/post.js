49010108b42e55ede9d8aaebc3000a95ae9ffe8d
pdhoward <patrick.howard@hotmail.com>
2020-06-02 16:42:29 -0400

    second blog post

---
+++ src/routes/posts/2020-01-08-conversations/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Conversations are hard`,
+  tags: ['marketing', 'sales', 'messaging'],
+  spoiler: "The holy grail of ai is ..",
+  getContent: () => import('./document.mdx'),
+}
