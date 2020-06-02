b08e1d11bbb249d3be48131e44c21ff7b4b674be
pdhoward <patrick.howard@hotmail.com>
2020-06-02 15:35:15 -0400

    add new post

--- src/routes/posts/2020-01-01-welcome/post.js
+++ src/routes/posts/2020-01-01-welcome/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: `The Audience of One!`,
+  title: `The Audience of One`,
   tags: ['marketing', 'sales', 'messaging'],
   spoiler: "Reaching your customers at the point of decision ..",
   getContent: () => import('./document.mdx'),
