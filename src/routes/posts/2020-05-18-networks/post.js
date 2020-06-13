c4d9ec724aa58537628f44278929ecc5e395e161
pdhoward <patrick.howard@hotmail.com>
2020-06-13 10:33:17 -0400

    updates

---
+++ src/routes/posts/2020-05-18-networks/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Invisible networks`,
+  tags: ['marketing', 'networks', 'algorithm'],
+  spoiler: "Discovering the unexpected ...",
+  getContent: () => import('./document.mdx'),
+}
