d15f446a43c09562916705a4dc6543cc2036bfc1
pdhoward <patrick.howard@hotmail.com>
2020-06-16 11:57:46 -0400

    udpated post

---
+++ src/routes/posts/2020-06-01-connected/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Trusted and Connected Conversations`,
+  tags: ['marketing', 'content', 'context'],
+  spoiler: "Intersecting at the point of interest ...",
+  getContent: () => import('./document.mdx'),
+}
