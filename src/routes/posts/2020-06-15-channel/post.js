f16899d0348613f255609879d5e04098386a1486
pdhoward <patrick.howard@hotmail.com>
2020-07-03 13:50:45 -0400

    june update

---
+++ src/routes/posts/2020-06-15-channel/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Platform Innovation`,
+  tags: ['marketing', 'ad', 'venue'],
+  spoiler: "Innovation in reaching your audience while preserving brand safety ...",
+  getContent: () => import('./document.mdx'),
+}
