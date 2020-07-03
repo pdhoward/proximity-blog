ed16b76d1154badce62d37a1d50c10a4d44ce69b
pdhoward <patrick.howard@hotmail.com>
2020-07-03 16:31:58 -0400

    final june update

---
+++ src/routes/posts/2020-06-24-pwa/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Venue Web App`,
+  tags: ['marketing', 'software', 'pwa'],
+  spoiler: "An app experience for every customer in every venue through the web ...",
+  getContent: () => import('./document.mdx'),
+}
