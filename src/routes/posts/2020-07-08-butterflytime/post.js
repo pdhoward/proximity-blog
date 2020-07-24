b243adcbc74666a7100c6a26781245058b03265c
pdhoward <patrick.howard@hotmail.com>
2020-07-24 13:06:29 -0400

    july post

---
+++ src/routes/posts/2020-07-08-butterflytime/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Global Reach. Local Experience`,
+  tags: ['marketing', 'software', 'pwa'],
+  spoiler: "A web app for every venue ...",
+  getContent: () => import('./document.mdx'),
+}
