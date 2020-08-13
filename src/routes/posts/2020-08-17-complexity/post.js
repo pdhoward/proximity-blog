e3b65883b72b585c105a17c28b6d42b5a51ae71b
pdhoward <patrick.howard@hotmail.com>
2020-08-13 15:40:31 -0400

    new blog post

---
+++ src/routes/posts/2020-08-17-complexity/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Thinking About Complexity`,
+  tags: ['marketing', 'time', 'segment'],
+  spoiler: "There is a choice function that helps us address complexity ...",
+  getContent: () => import('./document.mdx'),
+}
