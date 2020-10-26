ebff9a084fae99f88c6d70de1c37b1a99c445f26
pdhoward <patrick.howard@hotmail.com>
2020-10-26 14:12:54 -0400

    blog post update

---
+++ src/routes/posts/2020-09-25-forecast/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `The Next Big Thing in Technology`,
+  tags: ['change', 'forecast', 'tech'],
+  spoiler: "Forecasting change in a tsunami...",
+  getContent: () => import('./document.mdx'),
+}
---
+++ src/routes/posts/2020-10-29-sensing/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Hyperconnected`,
+  tags: ['network', 'time'],
+  spoiler: "How should governance work in a networked world...",
+  getContent: () => import('./document.mdx'),
+}
