42c9b695ae3e62a06e8488c1d219c1697a7dddf8
pdhoward <patrick.howard@hotmail.com>
2020-12-04 13:23:17 -0500

    update

---
+++ src/routes/posts/2020-12-04-physicalweb/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Your Product is Calling`,
+  tags: ['sensors', 'network', 'IOT'],
+  spoiler: "Disrupting the value chain...",
+  getContent: () => import('./document.mdx'),
+}
