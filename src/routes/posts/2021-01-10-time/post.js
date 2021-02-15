65981d4b3b2c7c04e784434af1d28ef920357b2b
pdhoward <patrick.howard@hotmail.com>
2021-02-15 13:37:07 -0500

    january post

---
+++ src/routes/posts/2021-01-10-time/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Time-based Competition`,
+  tags: ['sensors', 'network', 'IOT'],
+  spoiler: "Real-time product streaming changes everything...",
+  getContent: () => import('./document.mdx'),
+}
